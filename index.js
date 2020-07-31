dc961bc3f4a2dc0c3326f43bf20dfbe42192bd89
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 01:57:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
+
 function updateObjectWithKeyAndValue(object, key, value) {
-  return
-  Object.assign({}, object, {[key] : value})
+  return Object.assign({}, object, {[key] : value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
